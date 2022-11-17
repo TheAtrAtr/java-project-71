d4dde2e203768c66df66f0bcfbb0b5ae6404d749
TheAtrAtr <dyadya_atr@mail.ru>
2022-11-17 16:01:10 +0300

    4 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,22 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 
 @Command(name = "gendiff", version = "1.0", header = "",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable {
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File file1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File file2;
 
     @Option(names = {"-h", "--help"}, usageHelp = true,
             description = "Show this help message and exit.")
@@ -18,6 +25,9 @@ class App implements Callable {
     @Option(names = {"-V", "--version"}, versionHelp = true,
             description = "Print version information and exit.")
     boolean versionHelpRequested;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    File archive;
 
 
     public static void main(String... args) {
